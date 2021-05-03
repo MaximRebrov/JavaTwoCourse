f2f0db2d9665aef35304e00f0be2b1fe9661d041
MaximRebrov <81246462+MaximRebrov@users.noreply.github.com>
2021-05-04 00:47:05 +0300

    Delete lessons/src/main/java/lesson2 directory

--- lessons/src/main/java/lesson2/MyArrayDataException.java
+++
@@ -1,9 +0,0 @@
-package lesson2;
-
-public class MyArrayDataException extends Exception{
-
-    MyArrayDataException(int i, int j) {
-
-        System.out.println(String.format("Сannot be converted to int [%d][%d]", i, j));
-    }
-}
