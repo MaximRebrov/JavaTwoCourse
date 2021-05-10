fcb0f8502bd8d7d5362c47b9fcea7c72ce826d5a
Max <mr.rebrov2013@yandex.ru>
2021-05-10 22:11:45 +0300

    add lesson five

---
+++ lessons/src/main/java/lesson5/lesson5.java
@@ -0,0 +1,11 @@
+package lesson5;
+
+public class lesson5 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Array array = new Array();
+        array.arrayFull();
+        array.arrayHalf();
+    }
+}
